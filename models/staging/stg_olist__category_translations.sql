ebf632d99443dab7b8deb4d5d8657ec1a109e56a
binturongg <johnjephwolf@gmail.com>
2024-08-27 12:20:45 -0400

    Added source/staging: category_translations

---
+++ models/staging/stg_olist__category_translations.sql
@@ -0,0 +1,14 @@
+with source as (
+    select *
+    from {{ source('olist', 'product_category_name_translation')}}
+),
+
+renamed as (
+    select
+        product_category_name as category_name_portuguese,
+        product_category_name_english as category_name_english
+    from source
+)
+
+select *
+from renamed
